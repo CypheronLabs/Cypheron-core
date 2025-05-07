c73ca87be1c3a9a89180ad883ad58b3f42cf5091
mluna030 <michael.luna37@gmail.com>
2025-05-07 16:45:02 -0700

    Add: Sphincs128f

--- core-lib/src/sig/mod.rs
+++ core-lib/src/sig/mod.rs
@@ -2,8 +2,12 @@ pub mod falcon;
 pub mod sphincs;         
 pub mod traits;          
 pub mod dilithium;       
+
 pub use dilithium::dilithium2::Dilithium2; 
 pub use dilithium::dilithium3::Dilithium3; 
 pub use dilithium::dilithium5::Dilithium5;
+
+pub use falcon::falcon512::Falcon512;
+pub use falcon::falcon1024::Falcon1024;
 
 pub use traits::SignatureScheme;
---
+++ core-lib/src/sig/sphincs/mod.rs
@@ -0,0 +1,5 @@
+pub mod common;
+pub mod shake_128f;
+pub mod sha2_256s;
+//pub mod dilithium5;
+mod errors;
